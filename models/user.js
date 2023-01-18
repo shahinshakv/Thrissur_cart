8c3ea29ec0cd978d1b3bf55c292dfa63920493f7
SHAHINSHA <shahinshasaidu50@gmail.com>
2023-01-18 23:15:26 +0400

    Added country

--- models/user.js
+++ models/user.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const userSchema = mongoose.Schema({
   email : {type : String, required : true, unique: true },
-  password: {type : String, required : true }
+  password: {type : String, required : true },
+  name: {type : String, required : true},
+  flat: {type: String, required : true },
+  address: {type: String, required : true },
+  city: {type: String, required : true},
+  latitude: {type: String, required : true},
+  longitute: {type: String, required : true},
+  country: {type: String, required : true},
+
+
 });
 
 userSchema.plugin(uniqueValidator);
